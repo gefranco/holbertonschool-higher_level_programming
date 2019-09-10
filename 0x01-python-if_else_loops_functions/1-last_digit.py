6365d65c20aa567d40d9f7cad23fea44206e90ce
German Franco F <933@holbertonschool.com>
2019-09-10 19:58:28 +0000

    print the last digit of the number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+
+if number < 0:
+    last_digit *= -1
+
+print("Last digit of {:d} is {:d} and is".format(number,last_digit),end = " ")
+if last_digit == 0:
+    print("0")
+elif (last_digit < 6 ):
+    print("less than 6 and not 0".format(last_digit))
+else:
+    print("greater than 5")
