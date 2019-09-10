7a23d0a4049a381090816f7f38a5d51a1995f4e1
German Franco F <933@holbertonschool.com>
2019-09-10 20:05:14 +0000

    [Fix]print the last digit of the number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number < 0:
 print("Last digit of {:d} is {:d} and is".format(number,last_digit),end = " ")
 if last_digit == 0:
     print("0")
-elif (last_digit < 6 ):
+elif last_digit < 6:
     print("less than 6 and not 0".format(last_digit))
 else:
     print("greater than 5")
