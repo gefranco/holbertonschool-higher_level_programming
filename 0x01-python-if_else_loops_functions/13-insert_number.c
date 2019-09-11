fccb5c012d67a8cbf074f58ee0d06007fc4f99c9
German Franco F <933@holbertonschool.com>
2019-09-11 00:30:57 +0000

    [Fix] a function in C that inserts a node into a sorted singly linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
 /**
  * insert_node - insert a node in alinked list
  * @head: the pointer to the head of the list
@@ -10,42 +9,40 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-
 	listint_t *list_tmp = *head;
 	listint_t *list = list_tmp->next;
 	listint_t *end;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
-
 	if (number < list_tmp->n)
 	{
-
 		listint_t *insert = malloc(sizeof(listint_t));
+		if (insert == NULL)
+			return (NULL);
 		*head = insert;
 		insert->next = list_tmp;
 		insert->n = number;
 		return (insert);
-
 	}
 	while (list)
 	{
-
 		if (number < list->n)
 		{
 			listint_t *insert = malloc(sizeof(listint_t));
-
+			if (insert == NULL)
+				return (NULL);
 			list_tmp->next = insert;
 			insert->next = list;
 			insert->n = number;
 			return (insert);
 		}
-
 		list_tmp = list;
 		list = list->next;
-
 	}
 	end = malloc(sizeof(listint_t));
+	if (end == NULL)
+		return (NULL);
 	list_tmp->next = end;
 	end->next = NULL;
 	end->n = number;
