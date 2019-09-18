ee16614063b68c2b5be682a923a61af4c33445d6
German Franco F <933@holbertonschool.com>
2019-09-18 23:14:14 +0000

    [Fix]a function that adds 2 tuples.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,8 @@ def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
         return
 
-    my_list.reverse()
+    rev_list = my_list
+    rev_list.reverse()
 
-    for i in my_list:
+    for i in rev_list:
         print("{:d}".format(i))
