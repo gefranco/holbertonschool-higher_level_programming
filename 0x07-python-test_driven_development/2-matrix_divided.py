c9298334fb5ea5b9671c737e5d5eb40ed21aaa58
German Franco F <933@holbertonschool.com>
2019-10-04 02:52:07 +0000

    a function that divides all elements of a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+"2-matrix_divide" module.
+
+The 2-matrix_divide module supplies the function  matrix_divided(matrix, div):
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Function that divides all elements of a matrix
+    """
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div is 0:
+        raise ZeroDivisionError("division by zero")
+    matriz_result = []
+    row_result = []
+    row_length = len(matrix[0])
+    for row in matrix:
+        if row_length != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        row_result = []
+        for element in row:
+            if not isinstance(element, (int, float)):
+                msg = "matrix must be a matrix (list of lists)"
+                msg += "of integers/floats"
+                raise TypeError(msg)
+            row_result.append(round((element / div), 2))
+        matriz_result.append(row_result)
+    return matriz_result
