8c08fc71b94acdf2b09c340f14d4d47a8126efca
German Franco F <933@holbertonschool.com>
2019-10-16 23:27:57 +0000

    [fix]

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -22,6 +22,7 @@ class Student:
             return new_dic
 
     def reload_from_json(self, json):
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        if json is not None:
+            self.first_name = json["first_name"]
+            self.last_name = json["last_name"]
+            self.age = json["age"]
