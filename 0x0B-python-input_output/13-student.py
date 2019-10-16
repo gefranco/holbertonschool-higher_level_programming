f73b6e12d7c00d4e6bf9ff5d49bf1b495e12abb6
German Franco F <933@holbertonschool.com>
2019-10-16 23:33:07 +0000

    [Fix]

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -23,6 +23,9 @@ class Student:
 
     def reload_from_json(self, json):
         if json is not None:
-            self.first_name = json["first_name"]
-            self.last_name = json["last_name"]
-            self.age = json["age"]
+            if "first_name" in json:
+                self.first_name = json["first_name"]
+            if "last_name" in json:
+                self.last_name = json["last_name"]
+            if "age" in json:
+                self.age = json["age"]
