0874ffa4e9f5529e62c54a0ccafda29dd61907d8
German Franco F <933@holbertonschool.com>
2019-10-17 20:23:06 +0000

    [Fix]

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,8 +13,10 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
+
         super().integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
     def __str__(self):
-        return "[Square] " + str(self.__width) + "/" + str(self.__height)
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
