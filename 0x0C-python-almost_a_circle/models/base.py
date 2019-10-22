b08a0eedcc0801f9f54cfb56f36c30402f1dcf9d
German Franco F <933@holbertonschool.com>
2019-10-22 22:45:33 +0000

    [fix]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,7 +50,8 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         '''returns the list of the JSON string representation json_string'''
-
+        if json_string is None or len(json_string) == 0:
+            return []
         return json.loads(json_string)
 
     @classmethod
