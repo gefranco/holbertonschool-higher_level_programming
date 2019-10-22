5f1832dffedeaf72fe77ff69beb1f451f3e6636b
German Franco F <933@holbertonschool.com>
2019-10-22 20:33:27 +0000

    [Add] comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,9 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        constructor method
+        """
         if id is not None:
             self.id = id
         else:
