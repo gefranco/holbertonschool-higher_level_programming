b6481d0551944a0428b8592ef1841a8d2461f2b1
German Franco F <933@holbertonschool.com>
2019-10-22 22:34:58 +0000

    [fix]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,8 +26,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         '''returns the JSON string representation of list_dictionaries'''
-        if list_dictionaries is None or len(list_dictionaries) < 1:
-            return []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
 
         strjson = json.dumps(list_dictionaries)
         return strjson
