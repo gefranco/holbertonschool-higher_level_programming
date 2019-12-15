ff4d75e5bce17f5a327d42e31d3b9b7c1b7617ae
German Franco F <933@holbertonschool.com>
2019-12-15 15:17:24 +0000

    [add] 3. SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+prevent SQL Injection
+'''
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states where name = %s", [sys.argv[4]])
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
