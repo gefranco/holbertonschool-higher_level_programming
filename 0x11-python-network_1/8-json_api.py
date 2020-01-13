a52b57f3c3c2b06f9130e2e0de66e831eec82f0d
German Franco F <933@holbertonschool.com>
2020-01-13 21:37:17 +0000

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+-----------------------
+Python script that sends a POST request and displays the the body.
+-----------------------
+'''
+import requests
+import sys
+
+if __name__ == "__main__":
+    data = {}
+    data ['q'] = ''
+    if len(sys.argv) > 1:
+        data['q'] = sys.argv[1]
+    request = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    dic = eval(request.text)
+    if len(dic) > 0:
+        print('[{}] {}'.format(dic['id'], dic['name']))
+    else:
+        print('No Result')
