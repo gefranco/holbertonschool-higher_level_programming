69ccac1244f755a6413b50af36d7c065bd1e0b5e
German Franco F <933@holbertonschool.com>
2020-01-13 19:38:49 +0000

    [add] Python script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+-----------------------
+Python script that fetches https://intranet.hbtn.io/status
+-----------------------
+'''
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    print("Body Response:")
+    for line in response:
+        print('\t- type: ' + str(type(line)))
+        print('\t- utf8 content: ' + str(line.decode('utf-8')))
+        print('\t- content: ' + str(line))
