ea74c4f9ea91d4211f21fce0fb1abc2f9959f616
German Franco F <933@holbertonschool.com>
2020-01-13 19:52:16 +0000

    [fix]

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        print("Body Response:")
+        print("Body response:")
         body = response.read()
         print('\t- type: ' + str(type(body)))
         print('\t- content: ' + str(body))
