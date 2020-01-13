761c424d1200d80829c44aa9916365358d150635
German Franco F <933@holbertonschool.com>
2020-01-13 21:41:31 +0000

    [fix]

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,12 +9,15 @@ import sys
 
 if __name__ == "__main__":
     data = {}
-    data ['q'] = ''
+    data['q'] = ''
     if len(sys.argv) > 1:
         data['q'] = sys.argv[1]
-    request = requests.post('http://0.0.0.0:5000/search_user', data=data)
-    dic = eval(request.text)
-    if len(dic) > 0:
-        print('[{}] {}'.format(dic['id'], dic['name']))
-    else:
-        print('No Result')
+    try:
+        request = requests.post('http://0.0.0.0:5000/search_user', data=data)
+        dic = eval(request.text)
+        if len(dic) > 0:
+            print('[{}] {}'.format(dic['id'], dic['name']))
+        else:
+            print('No result')
+    except Exception as e:
+        pass
