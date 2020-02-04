935d2353431adc992947cfee3edc35bdaf07db36
gefranco <933@holbertonschool.com>
2020-02-04 21:49:58 +0000

    [fix]

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,12 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   let biggest = process.argv[2];
-  let secondBiggest = process.argv[2];
   for (let i = 2; i < process.argv.length; i++) {
     if (parseInt(process.argv[i]) > biggest) {
       biggest = parseInt(process.argv[i]);
     }
   }
+  let secondBiggest = 0;
 
   for (let i = 2; i < process.argv.length; i++) {
     if (parseInt(process.argv[i]) > secondBiggest && parseInt(process.argv[i]) < biggest) {
