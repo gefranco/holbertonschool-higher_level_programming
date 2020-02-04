e7ea02669a76fc28e5aae0d5491614a2926bc52f
gefranco <933@holbertonschool.com>
2020-02-04 21:35:46 +0000

    [fix] use parseInt

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,14 +6,14 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   let biggest = process.argv[2];
   let secondBiggest = process.argv[2];
   for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > biggest) {
-      biggest = process.argv[i];
+    if (parseInt(process.argv[i]) > biggest) {
+      biggest = parseInt(process.argv[i]);
     }
   }
 
   for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > secondBiggest && process.argv[i] < biggest) {
-      secondBiggest = process.argv[i];
+    if (parseInt(process.argv[i]) > secondBiggest && parseInt(process.argv[i]) < biggest) {
+      secondBiggest = parseInt(process.argv[i]);
     }
   }
   console.log(secondBiggest);
