cb189b26f8b5de0e2493db4e88d47d825ecdce14
gefranco <933@holbertonschool.com>
2020-02-10 22:15:48 +0000

    [add] 11. Factor index

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const list = require('./100-data').list;
+
+const newlist = list.map(function (num, index) {
+  return num * index;
+});
+
+console.log(list);
+console.log(newlist);
