451ae5a608d3711ad3cafd1af29e1cb4c260e056
gefranco <933@holbertonschool.com>
2020-02-11 23:32:21 +0000

    [fix]space

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
