7546e146fd3041b2fe61ca875628b8dd783ee8b9
gefranco <933@holbertonschool.com>
2020-02-11 23:42:28 +0000

    [fix]

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,9 +6,9 @@ let countCharacter = 0;
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     requestDict = JSON.parse(response.body);
-    const results = requestDict['results'];
+    const results = requestDict.results;
     for (let i = 0; i < results.length; i++) {
-      if (results[i]['characters'].includes('https://swapi.co/api/people/18/')) {
+      if (results[i].characters.includes('https://swapi.co/api/people/18/')) {
         countCharacter++;
       }
     }
