b2215964327980ba5cb0d1c3ef37a41edbd47e2a
gefranco <933@holbertonschool.com>
2020-02-13 00:14:41 +0000

    [add]

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,17 +6,13 @@ request(process.argv[2], function (error, response, body) {
   if (!error) {
     requestDict = JSON.parse(response.body);
     const tasks = {};
-    let userId = null;
-    let totalTasks = 0;
     for (let i = 0; i < requestDict.length; i++) {
-      if (userId !== requestDict[i].userId) {
-        totalTasks = 0;
-        userId = requestDict[i].userId;
-      }
       if (requestDict[i].completed) {
-        totalTasks++;
+        if (!tasks[requestDict[i].userId]) {
+          tasks[requestDict[i].userId] = 1;
+        }
+        tasks[requestDict[i].userId] += 1;
       }
-      tasks[requestDict[i].userId] = totalTasks;
     }
     console.log(tasks);
   }
